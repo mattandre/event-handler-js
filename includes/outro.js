68a09c6f6d545bb0301af6756d7a9bdb0cff429d
Matt Andre <mandre@colocrossing.com>
2014-09-18 22:59:41 -0400

    Add Global Exports for Event Listener and Event Object

--- includes/outro.js
+++ includes/outro.js
@@ -2,5 +2,7 @@
 	 * Global Exports
 	 */
 	global.EventHandler = exports.EventHandler;
+	global.EventListener = exports.EventListener;
+	global.EntityEvent = exports.EntityEvent;
 
 }(typeof window !== 'undefined' ? window : this));
