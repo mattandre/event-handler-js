272ffcb69dbc4370d5c51484c81368b50b3105b8
Matt Andre <mandre@colocrossing.com>
2014-09-18 23:10:20 -0400

    Store Pre-existing Class with the same name as a Export under the _existing attribute of the Export class. Fixes #7.

--- includes/outro.js
+++ includes/outro.js
@@ -1,8 +1,14 @@
 	/**
 	 * Global Exports
 	 */
-	global.EventHandler = exports.EventHandler;
-	global.EventListener = exports.EventListener;
-	global.EntityEvent = exports.EntityEvent;
+	var i, numExports, exportClasses = ['EventHandler', 'EventListener', 'EntityEvent'];
+
+	for (i = 0, numExports = exportClasses.length; i < numExports; i++) {
+		if (typeof global[exportClasses[i]] !== 'undefined') {
+			exports[exportClasses[i]]['_existing'] = global[exportClasses[i]];
+		}
+
+		global[exportClasses[i]] = exports[exportClasses[i]];
+	}
 
 }(typeof window !== 'undefined' ? window : this));
